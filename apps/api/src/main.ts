a66a896fa409c6f378a9ef9241dccb5e75982fc9
Tarek EL ASMI <33405200+tarekelasmi85@users.noreply.github.com>
2026-03-31 06:41:35 -0400

    Create main.ts with NestJS application bootstrap code

---
+++ apps/api/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  await app.listen(3000);
+}
+bootstrap();
